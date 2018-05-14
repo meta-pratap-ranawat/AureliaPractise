041fee6158c926b3a7ffb4e48af9706b4f85348d
Pratap Singh Ranawat <pratap.ranawat@metacube.com>
2018-05-14 20:33:37 +0530

    file-uplaod with delete function

--- file-upload/front/src/app.ts
+++ file-upload/front/src/app.ts
@@ -1,14 +1,18 @@
 import { inject, autoinject } from 'aurelia-framework'
 import { HttpClient } from 'aurelia-fetch-client'
+import 'font-awesome/css/font-awesome.css'
 @autoinject
 export class App {
-  message = 'Hello World!';
+  message: String;
+  savedImages: Array<any>;
   constructor(private http: HttpClient)
   {
     http.configure(config => {
       config.withBaseUrl('http://localhost:3000/');
     });
     this.http = http;
+    this.message = 'Hello World!';
+    this.savedImages = [];
   }
 
   submit(images)
@@ -25,8 +29,30 @@ export class App {
       body: formData
     })
     .then(response => response.json())
-    .then(data => console.log(data.message))
+    .then(data => {
+      console.log(data.message);
+    })
     .catch(error => console.log(error));
+
+  }
+
+  deleteImage(image)
+  {
+    let index = this.savedImages.indexOf(image);
+    if(index != -1)
+    {
+      this.savedImages.splice(index, 1);
+    }
+    
+  }
+
+  onFileChange(images)
+  { 
+    this.savedImages = [];
+    for(let i=0; i<images.length; i++)
+    {
+      this.savedImages.push(images[i]);
+    }
   }
 
 }
